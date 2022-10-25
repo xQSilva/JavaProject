dd7ca1399d4e04b4c2f86a23de54ac4f5a8f95b4
DavidWhitfield <davidmwhitfield1992@gmail.com>
2022-10-25 20:19:09 +0100

    First Commit

---
+++ src/main/java/com/cohart/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.cohart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("inside index()");
+		return "index";
+	}
+	
+	@RequestMapping("/registrationPage")
+	public String registration() {
+		
+		return "registrationPage";
+	}
+
+}
